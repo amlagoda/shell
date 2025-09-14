f162d547fbf82430a5021df4da6dae05682e5671
Alexander Lagoda <amlagoda@gmail.com>
2025-09-15 00:33:14 +1000

    [Print a prompt #oo8]

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,10 @@
 use std::io::{self, Write};
 
 fn main() {
-    // Uncomment this block to pass the first stage
-    // print!("$ ");
-    // io::stdout().flush().unwrap();
+    print!("$ ");
+    io::stdout().flush().unwrap();
 
     // Wait for user input
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
+    //let mut input = String::new();
+    //io::stdin().read_line(&mut input).unwrap();
 }
