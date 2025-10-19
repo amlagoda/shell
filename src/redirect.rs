9a0a9d52bcfd6c77c8c3f32e876a23da02dbbe54
Alexander Lagoda <amlagoda@gmail.com>
2025-10-19 21:23:46 +1000

    Builtin completion #qp2

---
+++ src/redirect.rs
@@ -0,0 +1,48 @@
+mod redirect {
+    fn is_redirect(arg: &str) -> bool {
+        [">", "1>", "2>", ">>", "1>>", "2>>"].contains(&arg)
+    }
+
+    fn normalize_redirect(redirect: &str) -> String {
+        if [">", ">>"].contains(&redirect) {
+            format!("1{}", redirect)
+        } else {
+            String::from(redirect)
+        }
+    }
+
+    fn parse_redirect(redirect: &str) -> [String; 2] {
+        let mut flow = String::new();
+        let mut mode = String::new();
+
+        for r in redirect.chars() {
+            if flow.len() == 0 {
+                flow.push(r);
+            } else {
+                mode.push(r);
+            }
+        }
+
+        [flow, mode]
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn test_is_redirect() {
+            assert!(is_redirect(">"));
+        }
+
+        #[test]
+        fn test_normalize_redirect() {
+            assert_eq!("1>".to_string(), normalize_redirect(">"));
+        }
+
+        #[test]
+        fn test_parse_redirect() {
+            assert_eq!(["2".to_string(), ">>".to_string()], parse_redirect("2>>"));
+        }
+    }
+}
