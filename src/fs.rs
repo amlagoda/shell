3761139992972bf8e7a6446284018771c759031d
Alexander Lagoda <amlagoda@gmail.com>
2025-10-21 17:01:52 +1000

    Builtin completion #qp2

--- src/fs.rs
+++ src/fs.rs
@@ -40,6 +40,30 @@ mod fs {
         use super::*;
         use std::env::current_dir;
         use std::fs::read_dir;
+        use std::io::Read;
+        use std::time::{SystemTime, UNIX_EPOCH};
+
+        #[test]
+        fn test_write_to_file() {
+            let path = SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap()
+                .subsec_millis();
+            let path = format!("/tmp/{}", path);
+
+            let r1 = write_to_file(path.as_str(), "Hello world!", false);
+            assert_eq!((), r1.unwrap());
+            let r2 = write_to_file(path.as_str(), "Good weather!", true);
+            assert_eq!((), r2.unwrap());
+
+            let mut file = OpenOptions::new()
+                .read(true)
+                .open(Path::new(&path))
+                .unwrap();
+            let mut r = String::new();
+            file.read_to_string(&mut r).unwrap();
+            assert_eq!("Hello world!Good weather!", r);
+        }
 
         #[test]
         fn test_is_executable_file() {
