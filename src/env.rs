e90d835ccd0006a37c698ac160a7ecad903766de
Alexander Lagoda <amlagoda@gmail.com>
2025-10-24 23:00:39 +1000

    Builtin completion #qp2

--- src/env.rs
+++ src/env.rs
@@ -4,25 +4,7 @@ mod env {
     fn split_env_path() -> Result<Vec<String>, VarError> {
         Ok(var("PATH")?
             .split(':')
-            .map(|r| String::from(r))
+            .map(|r| r.to_string())
             .collect::<Vec<String>>())
-    }
-
-    #[cfg(test)]
-    mod tests {
-        use super::*;
-        use std::env::set_var;
-
-        #[test]
-        fn test_split_env_path() {
-            let initial = var("PATH").unwrap();
-
-            set_var("PATH", "foo:bar");
-            assert_eq!(
-                vec!["foo".to_string(), "bar".to_string()],
-                split_env_path().unwrap()
-            );
-            set_var("PATH", initial);
-        }
     }
 }
