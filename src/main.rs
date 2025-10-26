1ee1054cb76e9bcd7dc7c6d521e94586d61c8c53
Alexander Lagoda <amlagoda@gmail.com>
2025-10-27 00:56:37 +1000

    Builtin completion #qp2

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,11 @@ use crate::env::env::split_env_path;
 use crate::fs::fs::write_to_file;
 use crate::keyboard::keyboard::handle_key;
 use crate::parser::parser::parse;
-use crossterm::{
-    cursor::MoveLeft,
-    event::read,
-    execute,
-    terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType},
-};
+use crossterm::cursor::MoveLeft;
+use crossterm::event::read;
+use crossterm::execute;
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType};
 use std::io::{stdout, Error, ErrorKind, Write};
-use std::process::ExitCode;
 
 mod command;
 mod env;
@@ -21,12 +18,6 @@ mod parser;
 fn main() -> Result<(), Error> {
     let mut stdout = stdout();
     let mut input = String::new();
-    let mut output: Option<String> = None;
-    let mut error: Option<String> = None;
-    let mut print: Option<String> = None;
-    let mut is_exit = false;
-    let mut is_enter = false;
-    let mut is_backspace = false;
 
     let r = split_env_path();
 
@@ -50,19 +41,21 @@ fn main() -> Result<(), Error> {
             continue;
         }
 
-        (input, print, is_enter, is_exit, is_backspace) = handle_key(input, &key.unwrap());
+        let (i, to_print, is_enter, mut is_exit, is_backspace) = handle_key(input, &key.unwrap());
+        input = i;
 
         if is_backspace {
             execute!(stdout, MoveLeft(1), Clear(ClearType::UntilNewLine))?;
         }
 
-        if print.is_some() {
-            write!(stdout, "{}", print.unwrap())?;
+        if to_print.is_some() {
+            write!(stdout, "{}", to_print.unwrap())?;
             stdout.flush()?;
-            print = None;
         }
 
         if is_enter {
+            let mut output: Option<String> = None;
+            let mut error = Some(String::from(": not found"));
             let (name, args, redirect) = parse(input.as_str());
 
             if name.is_some() {
@@ -71,9 +64,6 @@ fn main() -> Result<(), Error> {
                 let paths = bin_paths.iter().map(|r| r.as_str()).collect::<Vec<&str>>();
 
                 (output, error, is_exit) = command(name.as_str(), &args, &paths);
-            } else {
-                output = None;
-                error = Some(String::from(": not found"));
             }
 
             input.clear();
@@ -105,29 +95,26 @@ fn main() -> Result<(), Error> {
                 .filter(|r| r.len() > 0)
                 .map(|r| r.to_string())
                 .collect::<Vec<String>>();
-           
-            to_print.append(&mut output
-                .unwrap_or("".to_string())
-                .split("\n")
-                .into_iter()
-                .filter(|r| r.len() > 0)
-                .map(|r| r.to_string())
-                .collect::<Vec<String>>());
+
+            to_print.append(
+                &mut output
+                    .unwrap_or("".to_string())
+                    .split("\n")
+                    .into_iter()
+                    .filter(|r| r.len() > 0)
+                    .map(|r| r.to_string())
+                    .collect::<Vec<String>>(),
+            );
 
             for r in to_print.iter() {
                 write!(stdout, "\r\n{}", r)?;
                 stdout.flush()?;
             }
 
-            output = None;
-            error = None;
-
             if !is_exit {
                 write!(stdout, "\r\n$ ")?;
                 stdout.flush()?;
             }
-
-            is_enter = false;
         }
 
         if is_exit {
