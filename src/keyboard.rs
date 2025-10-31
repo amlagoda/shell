983e95cc12c0c4b7a1523c16a7f46d9d6df97489
Alexander Lagoda <amlagoda@gmail.com>
2025-11-01 02:50:48 +1000

    Missing completions #qm8

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -5,10 +5,12 @@ pub mod keyboard {
     pub fn handle_key(
         mut input: String,
         key: &KeyEvent,
+        previous_key: &Option<KeyEvent>,
         commands: &Vec<&str>,
         bin_paths: &Vec<&str>,
-    ) -> (String, Option<String>, bool, bool, bool) {
+    ) -> (String, Option<String>, Option<String>, bool, bool, bool) {
         let mut to_print: Option<String> = None;
+        let mut hint: Option<String> = None;
         let mut is_enter = false;
         let mut is_exit = false;
         let mut is_backspace = false;
@@ -24,13 +26,22 @@ pub mod keyboard {
             }
 
             KeyCode::Tab => {
-                let r = complete(input.as_str(), commands, bin_paths);
-
                 to_print = Some("\x07".to_string());
 
-                if let Some((Some(end), _)) = r {
-                    to_print = Some(format!("{} ", end));
-                    input.push_str(format!("{} ", end).as_str());
+                let r = complete(input.as_str(), commands, bin_paths);
+
+                if let Some((end, variants)) = r {
+                    if let Some(r) = variants {
+                        if let Some(f) = previous_key {
+                            if f.code == KeyCode::Tab {
+                                hint = Some(r.join("  "));
+                                to_print = None;
+                            }
+                        }
+                    } else if let Some(r) = end {
+                        input.push_str(format!("{} ", r).as_str());
+                        to_print = Some(format!("{} ", r));
+                    }
                 }
             }
 
@@ -51,7 +62,7 @@ pub mod keyboard {
             _ => {}
         }
 
-        (input, to_print, is_enter, is_exit, is_backspace)
+        (input, to_print, hint, is_enter, is_exit, is_backspace)
     }
 
     fn complete(
@@ -59,17 +70,39 @@ pub mod keyboard {
         commands: &Vec<&str>,
         paths: &Vec<&str>,
     ) -> Option<(Option<String>, Option<Vec<String>>)> {
-        if let Some((Some(end), _)) = complete_input(input, commands) {
-            return Some((Some(end), None));
+        let mut variants: Option<Vec<String>> = None;
+
+        if let Some((end, var)) = complete_input(input, commands) {
+            if end.is_some() {
+                return Some((end, None));
+            }
+
+            variants = var;
         }
 
-        let r = search_executable_files_in_paths(input, paths)?;
-        let r = r.iter().map(|r| r.as_str()).collect::<Vec<&str>>();
-        let r = paths_to_names(&r);
-        let variants = r.iter().map(|r| r.as_str()).collect::<Vec<&str>>();
+        if let Some(r) = search_executable_files_in_paths(input, paths) {
+            let r = r.iter().map(|r| r.as_str()).collect::<Vec<&str>>();
+            let r = paths_to_names(&r);
+            let names = r.iter().map(|r| r.as_str()).collect::<Vec<&str>>();
 
-        if let Some((Some(end), _)) = complete_input(input, &variants) {
-            Some((Some(end), None))
+            if let Some((end, var)) = complete_input(input, &names) {
+                if end.is_some() {
+                    return Some((end, None));
+                }
+
+                if let Some(mut r) = variants {
+                    r.append(&mut var.unwrap());
+                    variants = Some(r);
+                } else {
+                    variants = var;
+                }
+            }
+        }
+
+        if let Some(mut r) = variants {
+            r.sort_unstable();
+            r.dedup();
+            Some((None, Some(r)))
         } else {
             None
         }
@@ -98,7 +131,7 @@ pub mod keyboard {
         }
 
         if len == 1 {
-            let end = matches[0].replace(input, "");
+            let end = matches[0].replacen(input, "", 1);
             let matches = matches
                 .iter()
                 .map(|r| r.to_string())
@@ -133,6 +166,63 @@ pub mod keyboard {
     #[cfg(test)]
     mod tests {
         use super::*;
+        use std::env::current_dir;
+
+        #[test]
+        fn test_complete() {
+            assert_eq!(None, complete("", &vec!(), &vec!()));
+
+            assert_eq!(None, complete("foo", &vec!(), &vec!()));
+
+            assert_eq!(None, complete("", &vec!("foo"), &vec!()));
+
+            assert_eq!(None, complete("", &vec!(), &vec!("foo")));
+
+            assert_eq!(None, complete("", &vec!("foo"), &vec!("bar")));
+
+            let f = complete("foo", &vec!["bar"], &vec![]);
+            assert_eq!(None, f);
+
+            let r = "oo".to_string();
+            let f = complete("f", &vec!["foo"], &vec![]);
+            assert_eq!(Some((Some(r), None)), f);
+
+            let r = vec!["foo".to_string(), "fooo".to_string()];
+            let f = complete("f", &vec!["foo", "fooo"], &vec![]);
+            assert_eq!(Some((None, Some(r))), f);
+
+            let path = get_fixture_dir();
+
+            assert_eq!(None, complete("bar", &vec!(), &vec!(&path)));
+
+            let r = "xe".to_string();
+            let f = complete("e", &vec![], &vec![&path]);
+            assert_eq!(Some((Some(r), None)), f);
+
+            let r = vec!["foo".to_string(), "fooo".to_string()];
+            let f = complete("f", &vec![], &vec![&path]);
+            assert_eq!(Some((None, Some(r))), f);
+
+            let r = "ar".to_string();
+            let f = complete("b", &vec!["bar"], &vec![&path]);
+            assert_eq!(Some((Some(r), None)), f);
+
+            let r = "xe".to_string();
+            let f = complete("e", &vec!["bar"], &vec![&path]);
+            assert_eq!(Some((Some(r), None)), f);
+
+            let r = vec!["bar".to_string(), "barr".to_string()];
+            let f = complete("b", &vec!["bar", "barr"], &vec![&path]);
+            assert_eq!(Some((None, Some(r))), f);
+
+            let r = vec!["foo".to_string(), "fooo".to_string()];
+            let f = complete("f", &vec!["bar"], &vec![&path]);
+            assert_eq!(Some((None, Some(r))), f);
+
+            let r = vec!["foo".to_string(), "fooo".to_string()];
+            let f = complete("f", &vec!["foo", "fooo"], &vec![&path]);
+            assert_eq!(Some((None, Some(r))), f);
+        }
 
         #[test]
         fn test_paths_to_names() {
@@ -191,6 +281,16 @@ pub mod keyboard {
                 Some((Some(end), Some(matches))),
                 complete_input("foo", &vec!("foo", "fooo"))
             );
+        }
+
+        fn get_fixture_dir() -> String {
+            // ends with a slash
+            format!("{}/test/fixture/keyboard/", get_current_dir())
+        }
+
+        fn get_current_dir() -> String {
+            // does not end with a slash
+            current_dir().unwrap().to_str().unwrap().to_string()
         }
     }
 }
