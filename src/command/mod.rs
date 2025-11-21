eefbc505991da9e464770193a9cdc6407c52fe3b
Alexander Lagoda <amlagoda@gmail.com>
2025-11-21 20:44:14 +1000

    Dual-command pipeline #br6

--- src/command/mod.rs
+++ src/command/mod.rs
@@ -3,9 +3,82 @@ mod external;
 
 use crate::command::builtin::{run_builtin, to_builtin};
 use crate::command::external::{is_external, run_external};
+use crate::fs::write_to_file;
+use crate::parser::Parsed;
 use std::io::Error;
 
-pub fn run_command(
+pub fn run_commands(parseds: Vec<Parsed>, bin_paths: &Vec<&str>) -> Result<CommandResult, Error> {
+    let mut result = CommandResult::new(Some(": not found".to_string()), None);
+    let mut pipeline_args: Vec<String> = vec![];
+
+    for parsed in parseds {
+        let mut args = parsed.args().unwrap_or(vec![]);
+
+        if !pipeline_args.is_empty() {
+            let mut r = pipeline_args
+                .iter()
+                .map(|r| r.as_str())
+                .collect::<Vec<&str>>();
+
+            args.append(&mut r);
+        }
+
+        let temp = run_command(parsed.command(), &args, bin_paths)?;
+
+        if let Some(redirect) = parsed.redirect() {
+            let mut to_write = String::new();
+
+            if !redirect.is_stderr() && temp.output().is_some() {
+                to_write.push_str(temp.output().unwrap());
+            }
+
+            if redirect.is_stderr() && temp.error().is_some() {
+                to_write.push_str(temp.error().unwrap());
+            }
+
+            if !to_write.is_empty() {
+                to_write.push('\n');
+                write_to_file(redirect.path(), to_write.as_str(), redirect.is_append())?;
+            }
+        }
+
+        if temp.is_exit() {
+            if parsed.redirect().is_none() || parsed.redirect().unwrap().is_stderr() {
+                return Ok(temp);
+            }
+
+            return Ok(CommandResult::new(None, None));
+        }
+
+        if let Some(pipeline) = parsed.pipeline() {
+            let mut args = vec![];
+
+            if let Some(output) = temp.output() {
+                args.push(output.to_string());
+            }
+
+            if !pipeline.is_stdout() && temp.error().is_some() {
+                args.push(temp.error().unwrap().to_string());
+            }
+
+            pipeline_args = args;
+        }
+
+        if parsed.redirect().is_some() && parsed.pipeline().is_none() {
+            if parsed.redirect().unwrap().is_stderr() {
+                result = CommandResult::new(None, temp.output);
+            } else {
+                result = CommandResult::new(temp.error, None);
+            }
+        } else {
+            result = temp;
+        }
+    }
+
+    Ok(result)
+}
+
+fn run_command(
     command: &str,
     args: &Vec<&str>,
     bin_paths: &Vec<&str>,
