3e71c6e7a84bbeb35a945439cf6ab9e1d28c7f1c
Alexander Lagoda <amlagoda@gmail.com>
2025-12-03 14:01:46 +1000

    Dual-command pipeline #br6

--- src/env.rs
+++ src/env.rs
@@ -6,5 +6,12 @@ pub fn split_env_path() -> Result<Vec<String>, VarError> {
         .map(|r| r.to_string())
         .collect::<Vec<String>>())
 }
-
 // tested in command/mod.rs::test_command_from_paths
+
+pub fn get_env_builtin() -> Result<String, VarError> {
+    Ok(var(get_env_builtin_name())?)
+}
+
+pub fn get_env_builtin_name() -> String {
+    String::from("YOUR_PROGRAM_BUILTIN")
+}
