dbb06618ca3dba7237004a8c0c5a00a3b3af4462
Alexander Lagoda <amlagoda@gmail.com>
2025-12-10 19:53:02 +1000

    Dual-command pipeline #br6

---
+++ src/process.rs
@@ -0,0 +1,48 @@
+use std::io::{Error, PipeReader, PipeWriter};
+use std::process::{Child, Command as Process, Stdio};
+
+pub fn spawn_pipe_process(
+    bin: &str,
+    args: Option<&Vec<&str>>,
+    stdin: Option<PipeReader>,
+    stdout: Option<PipeWriter>,
+    stderr: Option<PipeWriter>,
+) -> Result<Child, Error> {
+    Ok(build_process(bin, args, stdin, stdout, stderr).spawn()?)
+}
+
+pub fn spawn_process(bin: &str, args: Option<&Vec<&str>>) -> Result<Child, Error> {
+    Ok(build_process(bin, args, None, None, None).spawn()?)
+}
+
+fn build_process(
+    bin: &str,
+    args: Option<&Vec<&str>>,
+    stdin: Option<PipeReader>,
+    stdout: Option<PipeWriter>,
+    stderr: Option<PipeWriter>,
+) -> Process {
+    let mut process = Process::new(bin);
+
+    process.stdin(Stdio::piped());
+    process.stdout(Stdio::piped());
+    process.stderr(Stdio::piped());
+
+    if let Some(args) = args {
+        process.args(args);
+    }
+
+    if let Some(stdin) = stdin {
+        process.stdin(stdin);
+    }
+
+    if let Some(stdout) = stdout {
+        process.stdout(stdout);
+    }
+
+    if let Some(stderr) = stderr {
+        process.stderr(stderr);
+    }
+
+    process
+}
