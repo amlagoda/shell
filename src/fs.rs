a0452805b194b8818ac3d5b127349151aef9e0a6
Alexander Lagoda <amlagoda@gmail.com>
2026-02-08 17:43:19 +1000

    Dual-command pipeline #br6

--- src/fs.rs
+++ src/fs.rs
@@ -1,19 +1,18 @@
-use std::fs::{read_dir, DirEntry, OpenOptions, ReadDir};
-use std::io::{Error, Write};
+use std::fs::{read_dir, DirEntry, File, OpenOptions, ReadDir};
+use std::io::Error;
 use std::os::unix::fs::PermissionsExt;
 use std::path::Path;
 
-pub fn write_to_file(path: &str, content: &str, append: bool) -> Result<(), Error> {
-    OpenOptions::new()
+pub fn open_file(path: &str, append: bool) -> Result<File, Error> {
+    let file = OpenOptions::new()
         .create(true)
         .read(true)
         .write(true)
         .truncate(!append)
         .append(append)
-        .open(Path::new(path))?
-        .write_all(content.as_bytes())?;
+        .open(Path::new(path))?;
 
-    Ok(())
+    Ok(file)
 }
 
 pub fn search_executable_file_in_paths(name: &str, paths: &Vec<&str>) -> Option<String> {
@@ -126,8 +125,6 @@ fn is_executable_file(entry: &DirEntry) -> Result<bool, Error> {
 mod tests {
     use super::*;
     use std::env::current_dir;
-    use std::io::Read;
-    use std::time::{SystemTime, UNIX_EPOCH};
 
     #[test]
     fn test_search_executable_file_in_paths() {
@@ -155,29 +152,6 @@ mod tests {
 
         let r = search_executable_files_in_paths("not", &paths);
         assert!(r.is_none());
-    }
-
-    #[test]
-    fn test_write_to_file() {
-        let name = SystemTime::now()
-            .duration_since(UNIX_EPOCH)
-            .unwrap()
-            .subsec_millis();
-        let path = format!("/tmp/{}", name);
-
-        let r1 = write_to_file(path.as_str(), "Hello world!", false);
-        assert_eq!((), r1.unwrap());
-        let r2 = write_to_file(path.as_str(), "Good weather!", true);
-        assert_eq!((), r2.unwrap());
-
-        let mut file = OpenOptions::new()
-            .read(true)
-            .open(Path::new(&path))
-            .unwrap();
-        let mut r = String::new();
-
-        file.read_to_string(&mut r).unwrap();
-        assert_eq!("Hello world!Good weather!", r);
     }
 
     fn get_fixture_dir() -> String {
