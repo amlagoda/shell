0352af71114fd19bdd2ccc2240ed8680e0fd486e
Alexander Lagoda <amlagoda@gmail.com>
2026-03-19 00:53:15 +1000

    Write history to file #in3

--- src/command/registry/history.rs
+++ src/command/registry/history.rs
@@ -1,5 +1,5 @@
 use crate::command::fmt::NewLine;
-use crate::fs::get_read_file;
+use crate::fs::{get_read_file, get_write_file};
 use crate::io::Stdio;
 use crate::storage::History;
 use std::io::{BufRead, BufReader, BufWriter, Error, Write};
@@ -10,52 +10,103 @@ pub fn run_command(
     history: &mut History,
     args: Option<&Vec<&str>>,
 ) -> Result<(), Error> {
-    let (limit, file_path) = get_args(args);
+    let validated = validate(args);
 
-    if let Some(file_path) = file_path {
-        return load_to_history(stdio, newline, history, file_path.as_str());
+    if let Err(err) = validated {
+        let msg = format!(
+            "{}history: {}{}",
+            newline.stderr_start(),
+            err,
+            newline.stderr_end()
+        );
+
+        write!(stdio.stderr(), "{}", msg)?;
+        stdio.stderr().flush()?;
+
+        return Ok(());
     }
 
-    if let Some(mut commands) = history.all() {
-        let mut buffer = BufWriter::with_capacity(4096, stdio.stdout());
-        let len_start = commands.len();
-        let mut len_limit = len_start;
-        let mut num = 1;
+    let (limit, loaders) = validated.unwrap();
 
-        if let Some(limit) = limit {
-            commands = commands.into_iter().rev().take(limit).rev().collect();
-            len_limit = commands.len();
+    if let Some(loaders) = loaders {
+        load_mode(stdio, newline, history, loaders)
+    } else {
+        print_mode(stdio, newline, history, limit)
+    }
+}
 
-            if limit > 0 && limit < len_start {
-                num = len_start - (limit - 1);
-            }
+fn print_mode(
+    stdio: &mut Stdio,
+    newline: &NewLine,
+    history: &mut History,
+    limit: Option<usize>,
+) -> Result<(), Error> {
+    let history = history.all();
+
+    if history.is_none() {
+        return Ok(());
+    }
+
+    let mut records = history.unwrap();
+    let mut buffer = BufWriter::with_capacity(4096, stdio.stdout());
+    let len_start = records.len();
+    let mut len_limit = len_start;
+    let mut num = 1;
+
+    if let Some(limit) = limit {
+        records = records.into_iter().rev().take(limit).rev().collect();
+        len_limit = records.len();
+
+        if limit > 0 && limit < len_start {
+            num = len_start - (limit - 1);
+        }
+    }
+
+    for (mut iter, command) in records.iter().enumerate() {
+        iter += 1;
+        let mut to_print = format!("    {}  {}", num, command);
+
+        if iter == 1 {
+            to_print = format!("{}{}", newline.stdout_start(), to_print);
         }
 
-        for (mut iter, command) in commands.iter().enumerate() {
-            iter += 1;
-            let mut to_print = format!("    {}  {}", num, command);
-
-            if iter == 1 {
-                to_print = format!("{}{}", newline.stdout_start(), to_print);
-            }
-
-            if iter == len_limit {
-                to_print = format!("{}{}", to_print, newline.stdout_end());
-            } else {
-                to_print = format!("{}\n", to_print);
-            }
-
-            buffer.write_all(to_print.as_bytes())?;
-            num += 1;
+        if iter == len_limit {
+            to_print = format!("{}{}", to_print, newline.stdout_end());
+        } else {
+            to_print = format!("{}\n", to_print);
         }
 
-        buffer.flush()?;
+        buffer.write_all(to_print.as_bytes())?;
+        num += 1;
+    }
+
+    buffer.flush()?;
+
+    Ok(())
+}
+
+fn load_mode(
+    stdio: &mut Stdio,
+    newline: &NewLine,
+    history: &mut History,
+    loaders: Vec<Loader>,
+) -> Result<(), Error> {
+    for loader in loaders {
+        match loader.operation {
+            Operation::Download => download(stdio, newline, history, loader.file_path.as_str())?,
+            Operation::Upload(UploadMode::Rewrite) => {
+                upload(history, loader.file_path.as_str(), false)?
+            }
+            Operation::Upload(UploadMode::Append) => {
+                upload(history, loader.file_path.as_str(), true)?
+            }
+        }
     }
 
     Ok(())
 }
 
-fn load_to_history(
+fn download(
     stdio: &mut Stdio,
     newline: &NewLine,
     history: &mut History,
@@ -85,23 +136,112 @@ fn load_to_history(
     Ok(())
 }
 
-fn get_args(args: Option<&Vec<&str>>) -> (Option<usize>, Option<String>) {
-    let mut limit = None;
-    let mut file_path = None;
+fn upload(history: &mut History, file_path: &str, append: bool) -> Result<(), Error> {
+    let history = history.all();
 
-    if let Some(args) = args {
-        let mut iter = args.into_iter();
+    if history.is_none() {
+        return Ok(());
+    }
 
-        while let Some(arg) = iter.next() {
-            if let Ok(parsed) = arg.parse::<usize>() {
-                limit = Some(parsed);
-            } else if arg == &"-r" {
-                if let Some(path) = iter.next() {
-                    file_path = Some(path.to_string());
-                }
+    let records = history.unwrap();
+    let mut file = get_write_file(file_path, append)?;
+    let mut buffer = BufWriter::with_capacity(4096, &mut file);
+
+    for record in records {
+        buffer.write_all(format!("{}\n", record).as_bytes())?;
+    }
+
+    buffer.flush()?;
+
+    Ok(())
+}
+
+fn validate(args: Option<&Vec<&str>>) -> Result<(Option<usize>, Option<Vec<Loader>>), Error> {
+    const LOAD_FLAGS: [&str; 3] = ["-r", "-w", "-a"];
+    const MODE_NOT_DEFINED: u8 = 0;
+    const MODE_PRINT: u8 = 1;
+    const MODE_LOAD: u8 = 2;
+    let mut mode = MODE_NOT_DEFINED;
+
+    let err = Error::other("incorrect arguments");
+    let mut loaders: Option<Vec<Loader>> = None;
+    let mut limit: Option<usize> = None;
+
+    if args.is_none() {
+        return Ok((limit, loaders));
+    }
+
+    let mut iter = args.unwrap().into_iter();
+    while let Some(arg) = iter.next() {
+        let arg = *arg;
+
+        if let Ok(parsed) = arg.parse::<usize>() {
+            if mode != MODE_NOT_DEFINED {
+                return Err(err);
             }
+
+            mode = MODE_PRINT;
+            limit = Some(parsed);
+        } else if LOAD_FLAGS.contains(&arg) {
+            if ![MODE_NOT_DEFINED, MODE_LOAD].contains(&mode) {
+                return Err(err);
+            }
+
+            mode = MODE_LOAD;
+            let operation = Operation::try_from(arg)?;
+            let file_path = iter.next();
+
+            if file_path.is_none() {
+                return Err(err);
+            }
+
+            let loader = Loader::new(file_path.unwrap().to_string(), operation);
+
+            loaders = if let Some(mut items) = loaders {
+                items.push(loader);
+                Some(items)
+            } else {
+                Some(vec![loader])
+            };
+        } else {
+            return Err(err);
         }
     }
 
-    (limit, file_path)
+    Ok((limit, loaders))
+}
+
+struct Loader {
+    file_path: String,
+    operation: Operation,
+}
+
+impl Loader {
+    fn new(file_path: String, operation: Operation) -> Loader {
+        Loader {
+            file_path,
+            operation,
+        }
+    }
+}
+
+enum Operation {
+    Download,
+    Upload(UploadMode),
+}
+
+impl Operation {
+    fn try_from(flag: &str) -> Result<Operation, Error> {
+        match flag {
+            "-r" => Ok(Operation::Download),
+            "-w" => Ok(Operation::Upload(UploadMode::Rewrite)),
+            "-a" => Ok(Operation::Upload(UploadMode::Append)),
+            _ => Err(Error::other("not supported")),
+        }
+    }
+}
+
+enum UploadMode {
+    Rewrite,
+    Append,
 }
