879aaf2831c7e41871471f253b2fca027823d164
Alexander Lagoda <amlagoda@gmail.com>
2026-03-19 23:51:49 +1000

    Append history to file #sx3

--- src/storage/struct.rs
+++ src/storage/struct.rs
@@ -1,3 +1,42 @@
+pub struct RevIter {
+    current: usize,
+    last: usize,
+    is_started: bool,
+}
+
+impl RevIter {
+    pub fn new(last: usize) -> RevIter {
+        RevIter {
+            current: last,
+            last: last,
+            is_started: false,
+        }
+    }
+
+    // move to start
+    pub fn next(&mut self) -> Option<usize> {
+        if !self.is_started {
+            self.is_started = true;
+            Some(self.current)
+        } else if self.current > 0 {
+            self.current -= 1;
+            Some(self.current)
+        } else {
+            None
+        }
+    }
+
+    // move to end
+    pub fn prev(&mut self) -> Option<usize> {
+        if self.current < self.last {
+            self.current += 1;
+            Some(self.current)
+        } else {
+            None
+        }
+    }
+}
+
 pub struct KeyValue {
     data: Vec<(String, usize)>,
 }
@@ -26,5 +65,45 @@ impl KeyValue {
         }
 
         self.data.push((key.to_string(), value));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_rev_iter() {
+        let mut rev_iter = RevIter::new(0);
+        assert_eq!(None, rev_iter.prev());
+        assert_eq!(Some(0), rev_iter.next());
+        assert_eq!(None, rev_iter.next());
+        assert_eq!(None, rev_iter.prev());
+
+        let mut rev_iter = RevIter::new(2);
+        assert_eq!(None, rev_iter.prev());
+        assert_eq!(Some(2), rev_iter.next());
+        assert_eq!(Some(1), rev_iter.next());
+        assert_eq!(Some(0), rev_iter.next());
+        assert_eq!(None, rev_iter.next());
+        assert_eq!(Some(1), rev_iter.prev());
+        assert_eq!(Some(2), rev_iter.prev());
+        assert_eq!(None, rev_iter.prev());
+        assert_eq!(Some(1), rev_iter.next());
+        assert_eq!(Some(0), rev_iter.next());
+        assert_eq!(None, rev_iter.next());
+    }
+
+    #[test]
+    fn test_key_value() {
+        let mut key_value = KeyValue::new();
+
+        key_value.set("key", 1);
+        assert_eq!(1, key_value.get("key").unwrap());
+
+        key_value.set("key", 2);
+        assert_eq!(2, key_value.get("key").unwrap());
+
+        assert_eq!(None, key_value.get("not exists"));
     }
 }
