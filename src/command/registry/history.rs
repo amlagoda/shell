e9561c8c0d77e80ea1942c7af42ee672127c2636
Alexander Lagoda <amlagoda@gmail.com>
2026-03-22 01:40:51 +1000

    Read history on startup #zp4

--- src/command/registry/history.rs
+++ src/command/registry/history.rs
@@ -1,8 +1,7 @@
 use crate::command::fmt::NewLine;
-use crate::fs::{get_read_file, get_write_file};
-use crate::history::Log as History;
+use crate::history::{download as history_download, upload as history_upload, Log as History};
 use crate::io::Stdio;
-use std::io::{BufRead, BufReader, BufWriter, Error, Write};
+use std::io::{BufWriter, Error, ErrorKind, Write};
 
 pub fn run_command(
     stdio: &mut Stdio,
@@ -79,10 +78,10 @@ fn load_mode(
         match loader.operation {
             Operation::Download => download(stdio, newline, history, loader.file_path.as_str())?,
             Operation::Upload(UploadMode::Rewrite) => {
-                upload(history, loader.file_path.as_str(), false)?
+                history_upload(history, loader.file_path.as_str(), false)?
             }
             Operation::Upload(UploadMode::Append) => {
-                upload(history, loader.file_path.as_str(), true)?
+                history_upload(history, loader.file_path.as_str(), true)?
             }
         }
     }
@@ -96,75 +95,29 @@ fn download(
     history: &mut History,
     file_path: &str,
 ) -> Result<(), Error> {
-    let file = get_read_file(file_path);
+    let msg = format!(
+        "{}history: {}: No such file or directory{}",
+        newline.stderr_start(),
+        file_path,
+        newline.stderr_end()
+    );
 
-    if file.is_err() {
-        let msg = format!(
-            "{}history: {}: No such file or directory{}",
-            newline.stderr_start(),
-            file_path,
-            newline.stderr_end()
-        );
+    let download = history_download(history, file_path);
 
+    if download.is_ok() {
+        return Ok(());
+    }
+
+    let err = download.unwrap_err();
+
+    if err.kind() == ErrorKind::NotFound {
         write!(stdio.stderr(), "{}", msg)?;
         stdio.stderr().flush()?;
 
-        return Ok(());
-    }
-
-    let buffer = BufReader::with_capacity(4096, file.unwrap());
-    for line in buffer.lines() {
-        history.add(line?);
-    }
-
-    Ok(())
-}
-
-fn upload(history: &mut History, file_path: &str, append: bool) -> Result<(), Error> {
-    let previous_index = if append {
-        history.get_upload_index(file_path)
+        Ok(())
     } else {
-        None
-    };
-
-    let (count, new_index) = upload_numbers(previous_index, history.len());
-
-    if new_index.is_none() {
-        return Ok(());
+        Err(err)
     }
-
-    let (records, _) = history.lasts(count);
-    let mut file = get_write_file(file_path, append)?;
-    let mut buffer = BufWriter::with_capacity(4096, &mut file);
-
-    for record in records {
-        buffer.write_all(format!("{}\n", record).as_bytes())?;
-    }
-
-    buffer.flush()?;
-    history.set_upload_index(file_path, new_index.unwrap());
-
-    Ok(())
-}
-
-fn upload_numbers(
-    previous_index: Option<usize>,
-    current_len: usize,
-) -> (Option<usize>, Option<usize>) {
-    let mut count = None;
-    let mut new_index = None;
-
-    if let Some(previous_index) = previous_index {
-        if current_len > (previous_index + 1) {
-            count = Some(current_len - (previous_index + 1));
-        }
-    }
-
-    if current_len > 0 {
-        new_index = Some(current_len - 1)
-    }
-
-    (count, new_index)
 }
 
 fn validate(args: Option<&Vec<&str>>) -> Result<(Option<usize>, Option<Vec<Loader>>), Error> {
@@ -255,39 +208,4 @@ impl Operation {
 enum UploadMode {
     Rewrite,
     Append,
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_upload_numbers() {
-        let (previous_index, current_len) = (None, 0);
-        assert_eq!((None, None), upload_numbers(previous_index, current_len));
-
-        let (previous_index, current_len) = (None, 1);
-        assert_eq!((None, Some(0)), upload_numbers(previous_index, current_len));
-
-        let (previous_index, current_len) = (None, 2);
-        assert_eq!((None, Some(1)), upload_numbers(previous_index, current_len));
-
-        let (previous_index, current_len) = (Some(0), 0);
-        assert_eq!((None, None), upload_numbers(previous_index, current_len));
-
-        let (previous_index, current_len) = (Some(0), 1);
-        assert_eq!((None, Some(0)), upload_numbers(previous_index, current_len));
-
-        let (previous_index, current_len) = (Some(0), 2);
-        assert_eq!(
-            (Some(1), Some(1)),
-            upload_numbers(previous_index, current_len)
-        );
-
-        let (previous_index, current_len) = (Some(1), 4);
-        assert_eq!(
-            (Some(2), Some(3)),
-            upload_numbers(previous_index, current_len)
-        );
-    }
 }
