a0d40f78183ab37cc2aa99e8901cca0a20ac45de
Alexander Lagoda <amlagoda@gmail.com>
2026-03-23 18:16:08 +1000

    Write history on exit #kz7

---
+++ src/structure/key_value.rs
@@ -0,0 +1,49 @@
+// for small collection
+pub struct KeyValue {
+    data: Vec<(String, usize)>,
+}
+
+impl KeyValue {
+    pub fn new() -> KeyValue {
+        KeyValue { data: vec![] }
+    }
+
+    pub fn get(&self, key: &str) -> Option<usize> {
+        for (inkey, value) in self.data.iter() {
+            if inkey == key {
+                return Some(*value);
+            }
+        }
+
+        None
+    }
+
+    pub fn set(&mut self, key: &str, value: usize) {
+        for (num, (inkey, _)) in self.data.iter().enumerate() {
+            if inkey == key {
+                self.data[num] = (key.to_string(), value);
+                return;
+            }
+        }
+
+        self.data.push((key.to_string(), value));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_key_value() {
+        let mut key_value = KeyValue::new();
+
+        key_value.set("key", 1);
+        assert_eq!(1, key_value.get("key").unwrap());
+
+        key_value.set("key", 2);
+        assert_eq!(2, key_value.get("key").unwrap());
+
+        assert_eq!(None, key_value.get("not exists"));
+    }
+}
---
+++ src/structure/mod.rs
@@ -0,0 +1,5 @@
+mod key_value;
+mod rev_iter;
+
+pub use key_value::KeyValue;
+pub use rev_iter::RevIter;
