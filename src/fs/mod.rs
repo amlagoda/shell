c2c2fa70b0ce510226deabc63da395fd7e99ae46
Alexander Lagoda <amlagoda@gmail.com>
2026-04-01 15:42:04 +1000

    File completion #zv2

--- src/fs/mod.rs
+++ src/fs/mod.rs
@@ -1,16 +1,14 @@
 mod find;
+mod write;
 
 use libc::{dup as c_dup, fcntl as c_fcntl, F_SETFL, O_NONBLOCK};
 use std::fs::{File, OpenOptions};
-use std::io::{Error, ErrorKind, Read, Write};
+use std::io::Error;
 use std::os::fd::FromRawFd;
 use std::path::Path;
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::Arc;
-use std::thread::sleep;
-use std::time::Duration;
 
 pub use find::{find_file, find_files};
+pub use write::transfer_data;
 
 pub fn to_nonblock_file(file_descriptor: u32) -> Result<File, Error> {
     let status = unsafe { c_fcntl(file_descriptor as i32, F_SETFL, O_NONBLOCK) };
@@ -26,78 +24,6 @@ pub fn to_nonblock_file(file_descriptor: u32) -> Result<File, Error> {
 
 pub fn to_independent_file(file_descriptor: u32) -> File {
     unsafe { File::from_raw_fd(c_dup(file_descriptor as i32)) }
-}
-
-pub fn transfer_data(
-    mut from: File,
-    mut to: File,
-    proceed: Arc<AtomicBool>,
-    mut skip_first_newline: bool,
-    memory_ordering: Ordering,
-) -> Result<(), Error> {
-    let mut buffer = [0; 4096];
-
-    loop {
-        match from.read(&mut buffer) {
-            Ok(read_bytes) => {
-                if read_bytes == 0 {
-                    break;
-                }
-
-                skip_first_newline = write_lines(&mut to, &buffer, read_bytes, skip_first_newline)?;
-                buffer = [0; 4096];
-            }
-            Err(err) => {
-                if err.kind() == ErrorKind::WouldBlock {
-                    if !proceed.load(memory_ordering) {
-                        while let Ok(read_bytes) = from.read(&mut buffer) {
-                            if read_bytes == 0 {
-                                break;
-                            }
-
-                            skip_first_newline =
-                                write_lines(&mut to, &buffer, read_bytes, skip_first_newline)?;
-                            buffer = [0; 4096];
-                        }
-                        break;
-                    }
-
-                    sleep(Duration::from_millis(10));
-                    continue;
-                }
-
-                // when running the tests uncategorized error
-                // not reproducible locally
-                return Ok(());
-            }
-        }
-    }
-
-    Ok(())
-}
-
-fn write_lines(
-    target: &mut File,
-    buffer: &[u8; 4096],
-    read_bytes: usize,
-    mut skip_first_newline: bool,
-) -> Result<bool, Error> {
-    let readed = String::from_utf8(buffer[..read_bytes].to_vec())
-        .map_err(|_| Error::other("from_utf8 error"))?;
-
-    // skip unnecessary newlines
-    for line in readed.split("\n").filter(|r| !["\n", "\0", ""].contains(r)) {
-        if skip_first_newline {
-            skip_first_newline = false;
-            write!(target, "{}", line)?;
-        } else {
-            write!(target, "\r\n{}", line)?;
-        }
-
-        target.flush()?;
-    }
-
-    Ok(skip_first_newline)
 }
 
 pub fn get_write_file(path: &str, append: bool) -> Result<File, Error> {
---
+++ src/fs/write.rs
@@ -0,0 +1,78 @@
+use std::fs::File;
+use std::io::{Error, ErrorKind, Read, Write};
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+use std::thread::sleep;
+use std::time::Duration;
+
+pub fn transfer_data(
+    mut from: File,
+    mut to: File,
+    proceed: Arc<AtomicBool>,
+    mut skip_first_newline: bool,
+    memory_ordering: Ordering,
+) -> Result<(), Error> {
+    let mut buffer = [0; 4096];
+
+    loop {
+        match from.read(&mut buffer) {
+            Ok(read_bytes) => {
+                if read_bytes == 0 {
+                    break;
+                }
+
+                skip_first_newline = write_lines(&mut to, &buffer, read_bytes, skip_first_newline)?;
+                buffer = [0; 4096];
+            }
+            Err(err) => {
+                if err.kind() == ErrorKind::WouldBlock {
+                    if !proceed.load(memory_ordering) {
+                        while let Ok(read_bytes) = from.read(&mut buffer) {
+                            if read_bytes == 0 {
+                                break;
+                            }
+
+                            skip_first_newline =
+                                write_lines(&mut to, &buffer, read_bytes, skip_first_newline)?;
+                            buffer = [0; 4096];
+                        }
+                        break;
+                    }
+
+                    sleep(Duration::from_millis(10));
+                    continue;
+                }
+
+                // when running the tests uncategorized error
+                // not reproducible locally
+                return Ok(());
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn write_lines(
+    target: &mut File,
+    buffer: &[u8; 4096],
+    read_bytes: usize,
+    mut skip_first_newline: bool,
+) -> Result<bool, Error> {
+    let readed = String::from_utf8(buffer[..read_bytes].to_vec())
+        .map_err(|_| Error::other("from_utf8 error"))?;
+
+    // skip unnecessary newlines
+    for line in readed.split("\n").filter(|r| !["\n", "\0", ""].contains(r)) {
+        if skip_first_newline {
+            skip_first_newline = false;
+            write!(target, "{}", line)?;
+        } else {
+            write!(target, "\r\n{}", line)?;
+        }
+
+        target.flush()?;
+    }
+
+    Ok(skip_first_newline)
+}
