aa9f9955c30186ebdf8ab3bb2afb8f9e9ae449f9
Alexander Lagoda <amlagoda@gmail.com>
2026-04-04 09:00:41 +1000

    Nested file completion #ue6

--- src/keyboard/mod.rs
+++ src/keyboard/mod.rs
@@ -1,7 +1,14 @@
-use crate::complete::complete_input;
+mod handle;
+
+use crate::keyboard::handle::handle_arrow;
+use crate::keyboard::handle::handle_backspace;
+use crate::keyboard::handle::handle_char;
+use crate::keyboard::handle::handle_enter;
+use crate::keyboard::handle::handle_tab;
+
 use crate::env::get_current_dir;
 use crate::history::Log;
-use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
+use crossterm::event::{KeyCode, KeyEvent};
 
 pub fn handle_key(
     input: &str,
@@ -15,86 +22,17 @@ pub fn handle_key(
     let mut handled_key = HandledKey::new(input.to_string(), has_user_typing);
 
     match key.code {
-        KeyCode::Enter => {
-            handled_key.is_enter = true;
-            handled_key.has_user_typing = false;
-        }
-
-        KeyCode::Backspace => {
-            if !handled_key.input.is_empty() {
-                handled_key.input.pop();
-                handled_key.backspace_len = Some(1);
-            }
-            // not else
-            if handled_key.input.is_empty() {
-                handled_key.has_user_typing = false;
-            }
-        }
-
-        KeyCode::Tab => {
-            handled_key.to_print = Some("\x07".to_string());
-
-            let r = complete_input(
-                handled_key.input.as_str(),
-                commands,
-                bin_paths,
-                get_current_dir().as_str(),
-            );
-
-            if let Some(completion) = r {
-                if let Some(r) = completion.get_variants() {
-                    if let Some(f) = previous_key {
-                        if f.code == KeyCode::Tab {
-                            handled_key.hint = Some(r.join("  "));
-                            handled_key.to_print = None;
-                        }
-                    }
-                } else {
-                    let selected = completion.get_selected().unwrap();
-
-                    handled_key.input.push_str(selected);
-                    handled_key.to_print = Some(selected.to_string());
-                }
-            }
-        }
-
-        KeyCode::Char(r) => {
-            let is_ctrl = key.modifiers == KeyModifiers::CONTROL;
-
-            if r == 'c' && is_ctrl {
-                handled_key.to_print = Some("^C".to_string());
-                handled_key.is_exit = true;
-            } else if r == 'j' && is_ctrl {
-                handled_key.is_enter = true;
-                handled_key.has_user_typing = false;
-            } else {
-                handled_key.input.push(r);
-                handled_key.to_print = Some(r.to_string());
-                handled_key.has_user_typing = true;
-            }
-        }
-
-        KeyCode::Up | KeyCode::Down => {
-            if !handled_key.has_user_typing {
-                if !handled_key.input.is_empty() {
-                    handled_key.backspace_len = Some(handled_key.input.len());
-                }
-
-                let command = if key.code == KeyCode::Up {
-                    log.next()
-                } else {
-                    log.prev()
-                };
-
-                if let Some(command) = command {
-                    handled_key.input = command;
-                    handled_key.to_print = Some(handled_key.input.clone());
-                } else {
-                    handled_key.to_print = Some(format!("{}\x07", handled_key.input));
-                }
-            }
-        }
-
+        KeyCode::Enter => handle_enter(&mut handled_key),
+        KeyCode::Backspace => handle_backspace(&mut handled_key),
+        KeyCode::Tab => handle_tab(
+            &mut handled_key,
+            commands,
+            bin_paths,
+            get_current_dir().as_str(),
+            previous_key,
+        ),
+        KeyCode::Char(r) => handle_char(&mut handled_key, &key, r),
+        KeyCode::Up | KeyCode::Down => handle_arrow(&mut handled_key, log, &key),
         _ => {}
     }
 
