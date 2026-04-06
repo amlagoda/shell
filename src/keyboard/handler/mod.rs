5d79bb3f01ed4b32d1800878729feaa8bb34cf5d
Alexander Lagoda <amlagoda@gmail.com>
2026-04-06 18:00:46 +1000

    Nested file completion #ue6

--- src/keyboard/handler/mod.rs
+++ src/keyboard/handler/mod.rs
@@ -1,63 +1,32 @@
-mod arrow;
-mod backspace;
-mod char;
-mod enter;
-mod tab;
+use crossterm::event::KeyEvent;
 
-pub use arrow::handle as handle_arrow;
-pub use backspace::handle as handle_backspace;
-pub use char::handle as handle_char;
-pub use enter::handle as handle_enter;
-pub use tab::handle as handle_tab;
-
-pub struct HandledKey {
-    input: String,
-    to_print: Option<String>,
-    hint: Option<String>,
-    backspace_len: Option<usize>,
-    is_enter: bool,
-    is_exit: bool,
-    has_user_typing: bool,
+struct HandledKey {
+    actions: Option<Vec<InteractiveAction>>,
+    previous: Option<KeyEvent>,
+    current: KeyEvent,
 }
 
 impl HandledKey {
-    pub fn new(input: String, has_user_typing: bool) -> HandledKey {
+    fn new(previous: Option<KeyEvent>, current: KeyEvent) -> HandledKey {
         HandledKey {
-            input,
-            to_print: None,
-            hint: None,
-            backspace_len: None,
-            is_enter: false,
-            is_exit: false,
-            has_user_typing,
+            previous,
+            current,
+            actions: None,
         }
     }
 
-    pub fn input(&self) -> &str {
-        self.input.as_str()
-    }
-
-    pub fn get_to_print(&self) -> Option<&str> {
-        self.to_print.as_deref()
-    }
-
-    pub fn get_hint(&self) -> Option<&str> {
-        self.hint.as_deref()
-    }
-
-    pub fn get_backspace_len(&self) -> Option<usize> {
-        self.backspace_len
-    }
-
-    pub fn is_enter(&self) -> bool {
-        self.is_enter
-    }
-
-    pub fn is_exit(&self) -> bool {
-        self.is_exit
-    }
-
-    pub fn has_user_typing(&self) -> bool {
-        self.has_user_typing
+    fn iter(&self) -> impl Iterator<Item = &InteractiveAction> {
+        self.actions.iter().flatten()
     }
 }
+
+enum InteractiveAction {
+    PrintInput(String),
+    PrintOutput(String),
+    SaveInput(String),
+    RemoveInput(usize),
+    Exit,
+}
+
+// let handled_key = HandledKey::new(previous, current);
+// handled_key.iter().next() -> &InteractiveAction
