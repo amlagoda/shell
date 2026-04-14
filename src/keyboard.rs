a1864351547be426780a61b25eb4d58656f24f1a
Alexander Lagoda <amlagoda@gmail.com>
2026-04-14 17:53:57 +1000

    Nested file completion #ue6

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -1,27 +1,27 @@
 use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
 
 pub fn to_action(key: &KeyEvent) -> Option<TerminalAction> {
-    match key.code {
-        KeyCode::Enter => Some(TerminalAction::Run),
-        KeyCode::Backspace => Some(TerminalAction::InputSub),
-        KeyCode::Tab => Some(TerminalAction::InputComplete),
+    let action = match key.code {
+        KeyCode::Enter => TerminalAction::Run,
+        KeyCode::Backspace => TerminalAction::InputSub,
+        KeyCode::Tab => TerminalAction::InputComplete,
         KeyCode::Char(r) => from_char(r, key.modifiers == KeyModifiers::CONTROL),
-        KeyCode::Up => Some(TerminalAction::HistoryNext),
-        KeyCode::Down => Some(TerminalAction::HistoryPrev),
-        _ => None,
-    }
+        KeyCode::Up => TerminalAction::HistoryNext,
+        KeyCode::Down => TerminalAction::HistoryPrev,
+        _ => return None,
+    };
+
+    Some(action)
 }
 
-fn from_char(symbol: char, is_ctrl: bool) -> Option<TerminalAction> {
-    let action = if symbol == 'c' && is_ctrl {
+fn from_char(symbol: char, is_ctrl: bool) -> TerminalAction {
+    if symbol == 'c' && is_ctrl {
         TerminalAction::Exit
     } else if symbol == 'j' && is_ctrl {
         TerminalAction::Run
     } else {
         TerminalAction::InputAdd(symbol)
-    };
-
-    Some(action)
+    }
 }
 
 pub enum TerminalAction {
