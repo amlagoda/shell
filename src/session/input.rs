341ee5430dd9bf5e439d76fde02b26bef77d443b
Alexander Lagoda <amlagoda@gmail.com>
2026-04-16 18:33:19 +1000

    Nested file completion #ue6

---
+++ src/session/input.rs
@@ -0,0 +1,91 @@
+pub struct Input {
+    data: String,
+    has_user_typing: bool,
+}
+
+impl Input {
+    pub fn new() -> Input {
+        Input {
+            data: String::new(),
+            has_user_typing: false,
+        }
+    }
+
+    fn data(&self) -> &str {
+        self.data.as_str()
+    }
+
+    fn has_user_typing(&self) -> bool {
+        self.has_user_typing
+    }
+
+    fn push(&mut self, data: &str, is_user_typing: bool) {
+        self.data.push_str(data);
+
+        if is_user_typing {
+            self.has_user_typing = true;
+        }
+    }
+
+    fn remove_last(&mut self, len: usize) {
+        if len == 0 || self.data.is_empty() {
+            return;
+        }
+
+        if len >= self.data.len() {
+            self.data = String::new();
+            self.has_user_typing = false;
+            return;
+        }
+
+        self.data = self
+            .data
+            .chars()
+            .take(self.data.chars().count() - len)
+            .collect();
+    }
+
+    fn clear(&mut self) {
+        self.data = String::new();
+        self.has_user_typing = false;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_input() {
+        let mut input = Input::new();
+
+        input.push("a", false);
+        assert_eq!("a", input.data());
+        assert_eq!(false, input.has_user_typing());
+
+        input.push("b", true);
+        assert_eq!("ab", input.data());
+        assert_eq!(true, input.has_user_typing());
+
+        input.push("c", false);
+        assert_eq!("abc", input.data());
+        assert_eq!(true, input.has_user_typing());
+
+        input.remove_last(0);
+        assert_eq!("abc", input.data());
+        assert_eq!(true, input.has_user_typing());
+
+        input.remove_last(1);
+        assert_eq!("ab", input.data());
+        assert_eq!(true, input.has_user_typing());
+
+        input.remove_last(2);
+        assert_eq!("", input.data());
+        assert_eq!(false, input.has_user_typing());
+
+        input.push("abc", true);
+        input.clear();
+        assert_eq!("", input.data());
+        assert_eq!(false, input.has_user_typing());
+    }
+}
