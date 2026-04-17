ea322c8dcdfce0189ffb74e389d44c40783925a0
Alexander Lagoda <amlagoda@gmail.com>
2026-04-18 04:33:24 +1000

    Nested file completion #ue6

---
+++ src/control/handler/history.rs
@@ -0,0 +1,42 @@
+use crate::cursor::move_left as cursor_move_left;
+use crate::history::Log;
+use crate::io::Stdio;
+use crate::session::State;
+use std::io::{Error, Write};
+
+pub fn handle(
+    direction: &Direction,
+    state: &mut State,
+    stdio: &mut Stdio,
+    log: &mut Log,
+) -> Result<(), Error> {
+    let input = state.terminal().input();
+
+    if input.has_user_typing() {
+        return Ok(());
+    }
+
+    let command = match direction {
+        Direction::Next => log.next(),
+        Direction::Prev => log.prev(),
+    };
+
+    if let Some(command) = command {
+        cursor_move_left(stdio.stdout(), input.get().unwrap().len() as u16)?;
+        write!(stdio.stdout(), "{}", command)?;
+
+        input.reset();
+        input.push_as_system(command.as_str());
+    } else {
+        write!(stdio.stdout(), "\x07")?;
+    }
+
+    stdio.stdout().flush()?;
+
+    Ok(())
+}
+
+pub enum Direction {
+    Next,
+    Prev,
+}
