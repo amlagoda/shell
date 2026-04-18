e316324830ee971e95dbb9aa61318799d6c2320f
Alexander Lagoda <amlagoda@gmail.com>
2026-04-18 13:58:23 +1000

    Nested file completion #ue6

---
+++ src/keyboard1/handler/backspace.rs
@@ -0,0 +1,12 @@
+// use crate::keyboard::handler::HandledKey;
+
+// pub fn handle(handled_key: &mut HandledKey) {
+// if !handled_key.input.is_empty() {
+// handled_key.input.pop();
+// handled_key.backspace_len = Some(1);
+// }
+// not else
+// if handled_key.input.is_empty() {
+//     handled_key.has_user_typing = false;
+// }
+// }
