7a75ee9ab8f91f1e43f815c329b945405e308709
Alexander Lagoda <amlagoda@gmail.com>
2026-04-24 19:31:46 +1000

    Nested file completion #ue6

---
+++ src/keyboard1/handler/enter.rs
@@ -0,0 +1,6 @@
+// use crate::keyboard::handler::HandledKey;
+
+// pub fn handle(handled_key: &mut HandledKey) {
+//     handled_key.is_enter = true;
+//     handled_key.has_user_typing = false;
+// }
