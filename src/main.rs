30b0ab0b140aff0d8096e05273c41b01f57d160a
Alexander Lagoda <amlagoda@gmail.com>
2026-04-25 00:20:26 +1000

    Nested file completion #ue6

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
 use crate::command::get_command_list;
-use crate::env::{get_args, get_current_dir, get_history_log_path, split_env_path};
-use crate::history::{download as download_log, upload as upload_log, Log};
-use crate::io::Stdio;
-// use crate::keyboard::handle_key;
 use crate::control::run;
-use crate::keyboard::to_action;
-use crate::parser::parse;
+use crate::env::{get_args, get_current_dir, get_history_log_path, split_env_path};
+use crate::history::{download as download_log, Log};
+use crate::io::Stdio;
 use crate::session::State;
-use crossterm::cursor::MoveLeft;
-use crossterm::event::{read, KeyEvent};
-use crossterm::execute;
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType};
+use crossterm::event::read;
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 use std::fs::File;
 use std::io::{stderr, stdin, stdout, Error, Write};
 use std::os::fd::{AsRawFd, FromRawFd};
@@ -61,10 +56,6 @@ fn run_interactive(
     log: &mut Log,
     bin_paths: &Vec<&str>,
 ) -> Result<(), Error> {
-    // let mut input = String::new();
-    // let mut previous_key: Option<KeyEvent> = None;
-    // let mut has_user_typing = false;
-
     enable_raw_mode()?;
     write!(stdio.stdout(), "\r$ ")?;
     stdio.stdout().flush()?;
@@ -93,68 +84,6 @@ fn run_interactive(
         if is_exit {
             break;
         }
-
-        /*let r = get_command_list();
-        let commands = r.iter().map(|r| r.as_str()).collect();
-
-        let handled_key = handle_key(
-            input.as_str(),
-            &key.unwrap(),
-            &previous_key.as_ref(),
-            &commands,
-            bin_paths,
-            log,
-            has_user_typing,
-        );
-
-        let mut is_exit = handled_key.is_exit();
-        previous_key = Some(key.unwrap());
-        input = handled_key.input().to_string();
-        has_user_typing = handled_key.has_user_typing();
-
-        if let Some(len) = handled_key.get_backspace_len() {
-            execute!(
-                stdio.stdout(),
-                MoveLeft(len as u16),
-                Clear(ClearType::UntilNewLine)
-            )?;
-        }
-
-        if let Some(r) = handled_key.get_to_print() {
-            stdio.stdout().flush()?;
-            write!(stdio.stdout(), "{}", r)?;
-            stdio.stdout().flush()?;
-        }
-
-        if let Some(r) = handled_key.get_hint() {
-            write!(stdio.stdout(), "\r\n{}\r\n$ {}", r, input)?;
-            stdio.stdout().flush()?;
-        }
-
-        if handled_key.is_enter() {
-            if let Some(parseds) = parse(input.as_str())? {
-                disable_raw_mode()?;
-                let parseds = parseds.iter().collect();
-                let output_starts_newline = true;
-                is_exit = run(&parseds, stdio, log, bin_paths, output_starts_newline)?;
-                enable_raw_mode()?;
-            }
-
-            input.clear();
-            log.reset();
-
-            if !is_exit {
-                write!(stdio.stdout(), "\r\n$ ")?;
-                stdio.stdout().flush()?;
-            }
-        }
-
-        if is_exit {
-            if let Some(file_path) = get_history_log_path() {
-                upload_log(log, file_path.as_str(), false)?;
-            }
-            break;
-        }*/
     }
 
     disable_raw_mode()?;
