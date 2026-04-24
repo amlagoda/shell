66e5744a0e044bca35ee60d01059c2afb5c65d0e
Alexander Lagoda <amlagoda@gmail.com>
2026-04-25 00:28:22 +1000

    Nested file completion #ue6

--- src/keyboard1/handler/char.rs
+++ src/keyboard1/handler/char.rs
@@ -1,18 +1,18 @@
-use crate::keyboard::handler::HandledKey;
-use crossterm::event::{KeyEvent, KeyModifiers};
+// use crate::keyboard::handler::HandledKey;
+// use crossterm::event::{KeyEvent, KeyModifiers};
 
-pub fn handle(handled_key: &mut HandledKey, key: &KeyEvent, r: char) {
-    let is_ctrl = key.modifiers == KeyModifiers::CONTROL;
+// pub fn handle(handled_key: &mut HandledKey, key: &KeyEvent, r: char) {
+//     let is_ctrl = key.modifiers == KeyModifiers::CONTROL;
 
-    if r == 'c' && is_ctrl {
-        handled_key.to_print = Some("^C".to_string());
-        handled_key.is_exit = true;
-    } else if r == 'j' && is_ctrl {
-        handled_key.is_enter = true;
-        handled_key.has_user_typing = false;
-    } else {
-        // handled_key.input.push(r);
-        // handled_key.to_print = Some(r.to_string());
-        // handled_key.has_user_typing = true;
-    }
-}
+//     // if r == 'c' && is_ctrl {
+//     handled_key.to_print = Some("^C".to_string());
+//     handled_key.is_exit = true;
+//     // } else if r == 'j' && is_ctrl {
+//     handled_key.is_enter = true;
+//     handled_key.has_user_typing = false;
+//     // } else {
+//     // handled_key.input.push(r);
+//     // handled_key.to_print = Some(r.to_string());
+//     // handled_key.has_user_typing = true;
+//     // }
+// }
