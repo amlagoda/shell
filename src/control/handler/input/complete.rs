0fe8dd399eff43ee84415da8bf728110e915e940
Alexander Lagoda <amlagoda@gmail.com>
2026-04-24 19:27:38 +1000

    Nested file completion #ue6

---
+++ src/control/handler/input/complete.rs
@@ -0,0 +1,66 @@
+use crate::complete::complete_input;
+use crate::io::Stdio;
+use crate::session::State;
+use crossterm::event::KeyCode;
+use std::io::{Error, Write};
+
+pub fn handle(
+    state: &mut State,
+    stdio: &mut Stdio,
+    commands: &Vec<&str>,
+    bin_paths: &Vec<&str>,
+    current_dir: &str,
+) -> Result<(), Error> {
+    let input = state.terminal().input().get();
+
+    if input.is_none() {
+        return not_found(stdio);
+    }
+
+    let completion = complete_input(input.unwrap(), commands, bin_paths, current_dir);
+
+    if completion.is_none() {
+        return not_found(stdio);
+    }
+
+    let completion = completion.unwrap();
+    let variants = completion.get_variants();
+
+    if variants.is_none() {
+        return one_found(completion.get_selected().unwrap(), stdio, state);
+    }
+
+    if let Some(previous_key) = state.keyboard().previous_key() {
+        if previous_key.code == KeyCode::Tab {
+            let input = state.terminal().input().get();
+            return more_found(input.unwrap(), variants.unwrap(), stdio);
+        }
+    }
+
+    not_found(stdio)
+}
+
+fn more_found(current: &str, found: Vec<&str>, stdio: &mut Stdio) -> Result<(), Error> {
+    let to_print = format!("\r\n{}\r\n$ {}", found.join("  "), current);
+
+    write!(stdio.stdout(), "{}", to_print)?;
+    stdio.stdout().flush()?;
+
+    Ok(())
+}
+
+fn one_found(found: &str, stdio: &mut Stdio, state: &mut State) -> Result<(), Error> {
+    write!(stdio.stdout(), "{}", found)?;
+    stdio.stdout().flush()?;
+
+    state.terminal().input().push_as_system(found);
+
+    Ok(())
+}
+
+fn not_found(stdio: &mut Stdio) -> Result<(), Error> {
+    write!(stdio.stdout(), "\x07")?;
+    stdio.stdout().flush()?;
+
+    Ok(())
+}
