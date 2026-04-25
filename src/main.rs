8c21e978a5dfac8ccefef814e6922f32406a6999
Alexander Lagoda <amlagoda@gmail.com>
2026-04-26 02:25:47 +1000

    Nested file completion #ue

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use self::control::{mode_command, mode_interactive};
-use self::env::{get_args, get_history_log_path, split_env_path};
+use self::env::{get_args, get_current_dir, get_history_log_path, split_env_path};
 use self::fmt::NewLine;
 use self::history::{download as download_history_log, History};
 use self::io::Stdio;
 use self::session::State;
+use self::setting::Setting;
 use std::io::Error;
 
 mod command;
@@ -20,16 +21,17 @@ mod keyboard;
 mod parser;
 mod process;
 mod session;
+mod setting;
 mod structure;
 
 fn main() -> Result<(), Error> {
     let mut stdio = Stdio::new();
     let mut state = State::new();
     let mut history = History::new();
-    let mut newline = NewLine::new();
+    let mut new_line = NewLine::new();
 
+    let current_dir = get_current_dir();
     let bin_paths = split_env_path()?;
-    let bin_paths = bin_paths.iter().map(|r| r.as_str()).collect();
     let args = get_args();
 
     if let Some(path) = get_history_log_path() {
@@ -37,14 +39,16 @@ fn main() -> Result<(), Error> {
     }
 
     if args.is_empty() {
-        newline.set_stdout_start(true);
-        newline.set_stderr_start(true);
+        new_line.set_stdout_start(true);
+        new_line.set_stderr_start(true);
 
-        mode_interactive(&mut state, &mut stdio, &mut history, &newline, &bin_paths)
+        let setting = Setting::from(new_line, bin_paths, current_dir);
+        mode_interactive(&mut state, &mut stdio, &mut history, &setting)
     } else {
         let input = args.join(" ");
         state.terminal().input().push_as_system(input.as_str());
 
-        mode_command(&mut state, &mut stdio, &mut history, &newline, &bin_paths)
+        let setting = Setting::from(new_line, bin_paths, current_dir);
+        mode_command(&mut state, &mut stdio, &mut history, &setting)
     }
 }
