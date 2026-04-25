a9a8a4cb3bfaeefb33eecccaf9882ce1889d13e0
Alexander Lagoda <amlagoda@gmail.com>
2026-04-26 01:28:28 +1000

    Nested file completion #u

--- src/control/mod.rs
+++ src/control/mod.rs
@@ -1,15 +1,75 @@
 mod handler;
 
+use crate::command::get_command_list;
 use crate::control::handler::{
     command, exit, history as history_get, input_add, input_complete, input_sub, HistoryDirection,
 };
+use crate::env::get_current_dir;
 use crate::fmt::NewLine;
 use crate::history::History;
 use crate::io::Stdio;
 use crate::keyboard::{to_action, TerminalAction};
 use crate::session::State;
+use crossterm::event::read as read_key;
 use crossterm::event::KeyEvent;
-use std::io::Error;
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use std::io::{Error, Write};
+
+pub fn mode_interactive(
+    state: &mut State,
+    stdio: &mut Stdio,
+    history: &mut History,
+    newline: &NewLine,
+    bin_paths: &Vec<&str>,
+) -> Result<(), Error> {
+    enable_raw_mode()?;
+    write!(stdio.stdout(), "\r$ ")?;
+    stdio.stdout().flush()?;
+
+    loop {
+        let pressed_key = read_key()?.as_key_event();
+
+        if pressed_key.is_none() {
+            continue;
+        }
+
+        let available_commands = get_command_list();
+        let available_commands = available_commands.iter().map(|r| r.as_str()).collect();
+
+        let is_exit = run_interactive(
+            &pressed_key.unwrap(),
+            state,
+            stdio,
+            history,
+            newline,
+            &available_commands,
+            bin_paths,
+            get_current_dir().as_str(),
+        )?;
+
+        state.keyboard().set_previous_key(pressed_key.unwrap());
+
+        if is_exit {
+            break;
+        }
+    }
+
+    disable_raw_mode()?;
+
+    Ok(())
+}
+
+pub fn mode_command(
+    state: &mut State,
+    stdio: &mut Stdio,
+    history: &mut History,
+    newline: &NewLine,
+    bin_paths: &Vec<&str>,
+) -> Result<(), Error> {
+    run_command(state, stdio, history, newline, bin_paths)?;
+
+    Ok(())
+}
 
 pub fn run_interactive(
     key: &KeyEvent,
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
-use crate::command::get_command_list;
-use crate::control::{run_command, run_interactive};
-use crate::env::{get_args, get_current_dir, get_history_log_path, split_env_path};
+use crate::control::{mode_command, mode_interactive};
+use crate::env::{get_args, get_history_log_path, split_env_path};
 use crate::fmt::NewLine;
 use crate::history::{download as download_history_log, History};
 use crate::io::Stdio;
 use crate::session::State;
-use crossterm::event::read as read_key;
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
-use std::io::{Error, Write};
+use std::io::Error;
 
 mod command;
 mod complete;
@@ -50,60 +47,4 @@ fn main() -> Result<(), Error> {
 
         mode_command(&mut state, &mut stdio, &mut history, &newline, &bin_paths)
     }
-}
-
-fn mode_interactive(
-    state: &mut State,
-    stdio: &mut Stdio,
-    history: &mut History,
-    newline: &NewLine,
-    bin_paths: &Vec<&str>,
-) -> Result<(), Error> {
-    enable_raw_mode()?;
-    write!(stdio.stdout(), "\r$ ")?;
-    stdio.stdout().flush()?;
-
-    loop {
-        let pressed_key = read_key()?.as_key_event();
-
-        if pressed_key.is_none() {
-            continue;
-        }
-
-        let available_commands = get_command_list();
-        let available_commands = available_commands.iter().map(|r| r.as_str()).collect();
-
-        let is_exit = run_interactive(
-            &pressed_key.unwrap(),
-            state,
-            stdio,
-            history,
-            newline,
-            &available_commands,
-            bin_paths,
-            get_current_dir().as_str(),
-        )?;
-
-        state.keyboard().set_previous_key(pressed_key.unwrap());
-
-        if is_exit {
-            break;
-        }
-    }
-
-    disable_raw_mode()?;
-
-    Ok(())
-}
-
-fn mode_command(
-    state: &mut State,
-    stdio: &mut Stdio,
-    history: &mut History,
-    newline: &NewLine,
-    bin_paths: &Vec<&str>,
-) -> Result<(), Error> {
-    run_command(state, stdio, history, newline, bin_paths)?;
-
-    Ok(())
 }
