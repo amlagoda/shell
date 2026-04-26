c7d46636e79e7ea1cdb63fc2aa048e9b503a501d
Alexander Lagoda <amlagoda@gmail.com>
2026-04-27 00:51:10 +1000

    Nested file completion #ue

--- src/control/handler/command.rs
+++ src/control/handler/command.rs
@@ -1,11 +1,9 @@
 use crate::core::run;
-use crate::env::get_history_log_path;
-use crate::history::{upload as upload_log, History};
+use crate::history::History;
 use crate::io::Stdio;
 use crate::parser::parse;
 use crate::session::State;
 use crate::setting::Setting;
-
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 use std::io::{Error, Write};
 
@@ -24,21 +22,16 @@ pub fn handle(
     let parseds = parse(input.unwrap())?.unwrap();
 
     disable_raw_mode()?;
-    let mut is_exit = run(&parseds.iter().collect(), stdio, history, setting)?;
+    let need_exit = run(&parseds.iter().collect(), stdio, history, setting)?;
     enable_raw_mode()?;
 
     state.terminal().input().reset();
     history.reset();
 
-    if is_exit {
-        if let Some(file_path) = get_history_log_path() {
-            upload_log(history, file_path.as_str(), false)?;
-        }
-        is_exit = true;
-    } else {
+    if !need_exit {
         write!(stdio.stdout(), "\r\n$ ")?;
         stdio.stdout().flush()?;
     }
 
-    Ok(is_exit)
+    Ok(need_exit)
 }
