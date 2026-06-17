2d8bb9cebb5dd7af45c7f4c33b1d4bfaeab800d4
Alexander Lagoda <amlagoda@gmail.com>
2026-06-17 21:32:46 +1000

    Nested file completion #ue

--- src/complete/mod.rs
+++ src/complete/mod.rs
@@ -100,7 +100,7 @@ fn complete_to_variants(starts_with: &str, variants: &Vec<&str>) -> Option<Compl
 
     let variants = found.iter().map(|r| r.as_str()).collect();
 
-    if let Some(prefixed) = get_prefixed_variant(starts_with, &variants) {
+    if let Some(prefixed) = get_prefixed_variant(starts_with, variants) {
         let selected = prefixed.replacen(starts_with, "", 1);
         Some(Completion::from_selected(selected))
     } else {
@@ -140,7 +140,7 @@ fn complete_path(find_data: &FileFindData) -> Option<Completion> {
 
     let variants = found.iter().map(|r| r.as_str()).collect();
 
-    if let Some(prefixed) = get_prefixed_variant(starts_with, &variants) {
+    if let Some(prefixed) = get_prefixed_variant(starts_with, variants) {
         let selected = prefixed.replacen(starts_with, "", 1);
         Some(Completion::from_selected(selected))
     } else {
@@ -201,12 +201,26 @@ fn paths_to_names(paths: &Vec<&str>) -> Vec<String> {
     names
 }
 
-fn get_prefixed_variant(current: &str, variants: &Vec<&str>) -> Option<String> {
+fn get_prefixed_variant(current: &str, mut variants: Vec<&str>) -> Option<String> {
     if variants.is_empty() {
         return None;
     }
 
-    let mut short = variants.iter().min_by_key(|r| r.len()).unwrap();
+    if variants.len() == 1 {
+        return Some(variants[0].to_string());
+    }
+
+    let short = if current.is_empty() {
+        variants.iter().min_by_key(|r| r.len()).unwrap()
+    } else {
+        variants = variants
+            .iter()
+            .filter(|r| **r != current)
+            .map(|r| *r)
+            .collect();
+
+        variants.iter().min_by_key(|r| r.len()).unwrap()
+    };
 
     let is_chain = variants
         .iter()
@@ -214,14 +228,6 @@ fn get_prefixed_variant(current: &str, variants: &Vec<&str>) -> Option<String> {
 
     if !is_chain {
         return None;
-    }
-
-    if !current.is_empty() {
-        short = variants
-            .iter()
-            .filter(|&r| r != &current)
-            .min_by_key(|r| r.len())
-            .unwrap();
     }
 
     Some(short.to_string())
@@ -344,21 +350,28 @@ mod tests {
     #[test]
     fn test_get_prefixed_variant() -> Result<(), Error> {
         let variants = vec![];
-        assert!(get_prefixed_variant("", &variants).is_none());
-        assert!(get_prefixed_variant("f", &variants).is_none());
+        assert!(get_prefixed_variant("", variants).is_none());
 
-        let variants = vec!["f", "fo", "b"];
-        assert!(get_prefixed_variant("", &variants).is_none());
-
-        let variants = vec!["fo", "foo", "fy"];
-        assert!(get_prefixed_variant("f", &variants).is_none());
-
-        let variants = vec!["fo", "fooo", "foo"];
-        assert_eq!("fo", get_prefixed_variant("", &variants).unwrap());
-        assert_eq!("foo", get_prefixed_variant("fo", &variants).unwrap());
+        let variants = vec![];
+        assert!(get_prefixed_variant("f", variants).is_none());
 
         let variants = vec!["f"];
-        assert_eq!("f", get_prefixed_variant("", &variants).unwrap());
+        assert_eq!("f", get_prefixed_variant("", variants).unwrap());
+
+        let variants = vec!["f"];
+        assert_eq!("f", get_prefixed_variant("f", variants).unwrap());
+
+        let variants = vec!["f", "fo", "b"];
+        assert!(get_prefixed_variant("", variants).is_none());
+
+        let variants = vec!["f", "fo", "foo"];
+        assert_eq!("f", get_prefixed_variant("", variants).unwrap());
+
+        let variants = vec!["f", "fo", "foo", "fy"];
+        assert!(get_prefixed_variant("f", variants).is_none());
+
+        let variants = vec!["f", "fo", "foo"];
+        assert_eq!("fo", get_prefixed_variant("f", variants).unwrap());
 
         Ok(())
     }
